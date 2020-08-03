ec070795025a43e542e928689a9759a7ed954a55
Micah Smith <micahs@mit.edu>
2020-08-03 15:39:40 -0400

    Revert "Move cli to __main__ module"
    This reverts commit 8aa5c16e62c1ebcde75fcb6f5854cbdfde3ed816.

--- src/ballet_predict_house_prices/features/__init__.py
+++ src/ballet_predict_house_prices/features/__init__.py
@@ -1,10 +1,13 @@
+import collections
 import logging
 
 import ballet
 import ballet.contrib
 import ballet.util.mod
+import click
 import numpy as np
-from ballet.pipeline import EngineerFeaturesResult, FeatureEngineeringPipeline
+from ballet.pipeline import BuildResult, FeatureEngineeringPipeline
+from ballet.util.io import save_features, save_targets
 from sklearn.preprocessing import FunctionTransformer
 from stacklog import stacklog
 
@@ -56,5 +59,34 @@ def build(X_df=None, y_df=None):
     encoder_y = get_target_encoder()
     y = encoder_y.fit_transform(y_df)
 
-    return EngineerFeaturesResult(X_df=X_df, features=features, mapper_X=mapper_X, X=X,
-                                  y_df=y_df, encoder_y=encoder_y, y=y)
+    return BuildResult(X_df=X_df, features=features, mapper_X=mapper_X, X=X,
+                       y_df=y_df, encoder_y=encoder_y, y=y)
+
+
+@click.command()
+@click.argument('input_dir', type=click.Path(
+    exists=True, file_okay=False, readable=True))
+@click.argument('output_dir', type=click.Path(file_okay=False))
+def main(input_dir, output_dir):
+    """Engineer features"""
+
+    import ballet.util.log
+    ballet.util.log.enable(logger=logger, level='INFO', echo=False)
+    ballet.util.log.enable(logger=ballet.util.log.logger, level='INFO',
+                           echo=False)
+
+    X_df, y_df = load_data(input_dir=input_dir)
+    out = build()
+
+    mapper_X = out.mapper_X
+    encoder_y = out.encoder_y
+
+    X_ft = mapper_X.transform(X_df)
+    y_ft = encoder_y.transform(y_df)
+
+    save_features(X_ft, output_dir)
+    save_targets(y_ft, output_dir)
+
+
+if __name__ == '__main__':
+    main()
