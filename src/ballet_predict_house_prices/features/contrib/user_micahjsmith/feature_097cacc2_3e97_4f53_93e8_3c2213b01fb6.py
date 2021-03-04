2155aa3b9b9340393d86b664f9f9eaf3d1367aef
micahjsmith <micahjsmith@users.noreply.github.com>
2021-03-04 16:51:02 +0000

    Add new feature

---
+++ src/ballet_predict_house_prices/features/contrib/user_micahjsmith/feature_097cacc2_3e97_4f53_93e8_3c2213b01fb6.py
@@ -0,0 +1,6 @@
+from ballet import Feature
+
+input = None  # TODO - str or list of str
+transformer = None  # TODO - function, transformer-like, or list thereof
+name = None  # TODO - str
+feature = Feature(input, transformer, name)
